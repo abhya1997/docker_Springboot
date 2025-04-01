443c0d0ab28cb07423a8b71c8fb16d4cb1ee6f59
Abhi <abhi84468@gmail.com>
2025-04-01 12:54:13 +0530

    Second Commit

--- src/test/java/com/example/DockerTest/DockerTestApplicationTests.java
+++ src/test/java/com/example/DockerTest/DockerTestApplicationTests.java
@@ -1,13 +1,19 @@
 package com.example.DockerTest;
 
 import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.logging.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DockerTestApplicationTests {
+	
+	public static Logger logger=org.slf4j.LoggerFactory.getLogger(DockerTestApplicationTests.class);
 
 	@Test
 	void contextLoads() {
+		logger.info("Hey Our First Test With Jenkins");
 	}
+	
 
 }
